8037cdc8cdb71d4dc24788e46e13ab030e8f0981
Tim Weber <scy@scytale.name>
2008-10-14 23:17:34 +0200

    is.gd URL shortening, Twitter calls now work.

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
-function fin($text) {
+function fin($text, $die = true) {
 	$fp = @fopen('log.txt', 'a');
 	if ($fp !== false) {
 		@fwrite($fp, date('r') . " $text\n");
 		fclose($fp);
 	}
-	die($text);
+	if ($die)
+		die($text);
 }
 
 file_put_contents('payload', $_POST['payload']);
@@ -20,10 +21,6 @@ if (!function_exists('curl_init'))
 	fin('cURL library not installed.');
 
 $json = @$_POST['payload'];
-
-// FIXME: Remove this.
-// For debugging purposes, get the data from a file.
-$json = file_get_contents('input');
 
 // Die if there's no payload.
 if ($json == null)
@@ -58,13 +55,27 @@ if (($user == null) || ($pass == null))
 	fin('Twitter configuration missing.');
 
 foreach ($data['commits'] as $commit) {
-	$text = 'Commit: ' . $commit['message'] . ' ' . $commit['url'];
+	$message = explode("\n", $commit['message'], 2);
+	$curl = curl_init('http://is.gd/api.php?longurl=' . urlencode($commit['url']));
+	if ($curl === false)
+		continue;
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$ret = trim(curl_exec($curl));
+	$rcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+	curl_close($curl);
+	if ($rcode != 200)
+		continue;
+	$text = 'Commit: ' . $message[0] . ' ' . $ret;
 	$curl = curl_init('https://twitter.com/statuses/update.json');
 	if ($curl === false)
 		continue;
+	fin('Twittering: ' . $text, false);
 	curl_setopt($curl, CURLOPT_POST, true);
 	curl_setopt($curl, CURLOPT_POSTFIELDS, 'status=' . urlencode($text));
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($curl, CURLOPT_USERPWD, $user . ':' . $pass);
+	curl_setopt($curl, CURLOPT_NOBODY, true);
+	curl_exec($curl);
 	curl_close($curl);
 }
 
