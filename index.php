6c507169d76a8f9253517ba70d46151349a58627
Tim Weber <scy@scytale.name>
2008-10-14 22:31:39 +0200

    Added logging function, \ stripping. Removed key code.
    The \ stripping is really scary, is there no standard function available in PHP for this?
    
    "Keys" don't work, because GitHub strips GET parameters in the web hook. I guess we should go via the file name of TweetHub.

--- index.php
+++ index.php
@@ -1,45 +1,53 @@
 <?php
 
-file_put_contents('debug', $_POST['payload']);
+function fin($text) {
+	$fp = @fopen('log.txt', 'a');
+	if ($fp !== false) {
+		@fwrite($fp, date('r') . " $text\n");
+		fclose($fp);
+	}
+	die($text);
+}
+
+file_put_contents('payload', $_POST['payload']);
 
 // Die if the JSON library is not available.
 if (!function_exists('json_decode'))
-	die('JSON library not installed.');
+	fin('JSON library not installed.');
 
 // Die if cURL is not available.
 if (!function_exists('curl_init'))
-	die('cURL library not installed.');
+	fin('cURL library not installed.');
 
 $json = @$_POST['payload'];
 
 // FIXME: Remove this.
 // For debugging purposes, get the data from a file.
-$json = file_get_contents('in.txt');
+$json = file_get_contents('input');
 
 // Die if there's no payload.
 if ($json == null)
-	die('No payload.');
+	fin('No payload.');
+
+// Remove escapes. This is really spooky.
+$data = str_replace(array("\\\"", "\\'", "\\\\/", "\\\\"), array('"', "'", '/', "\\"), $json);
 
 // Decode JSON to associative array.
-$data = json_decode($json, true);
+$data = json_decode($data, true);
 
 // Get project name.
 $proj = preg_replace('#^https?://(?:www\.)?github\.com/([^/]+)/([^/]+)/?$#', '$1/$2', $data['repository']['url']);
 
 // Die if it's not recognized.
 if (($proj === null) || ($proj == $data['repository']['url']))
-	die('Project name could not be read.');
+	fin('Project name could not be read.');
 
 // Load configuration.
 require_once('config.php');
 
 // Die if there's no such project configured.
 if (!array_key_exists($proj, $CONF))
-	die('No such project configured.');
-
-// Check the supplied key.
-if ($_GET['key'] != $CONF[$proj]['key'])
-	die('Wrong key.');
+	fin('No such project configured.');
 
 // Check if Twitter is configured for this project.
 $user = $CONF[$proj]['twitter']['user'];
@@ -47,7 +55,7 @@ $pass = $CONF[$proj]['twitter']['pass'];
 
 // Die if Twitter configuration is not complete.
 if (($user == null) || ($pass == null))
-	die('Twitter configuration missing.');
+	fin('Twitter configuration missing.');
 
 foreach ($data['commits'] as $commit) {
 	$text = 'Commit: ' . $commit['message'] . ' ' . $commit['url'];
@@ -57,7 +65,6 @@ foreach ($data['commits'] as $commit) {
 	curl_setopt($curl, CURLOPT_POST, true);
 	curl_setopt($curl, CURLOPT_POSTFIELDS, 'status=' . urlencode($text));
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-	curl_exec($curl);
 	curl_close($curl);
 }
 
